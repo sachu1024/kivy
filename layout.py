f133e56d1b607040abc02a4fb218fc32b2871d2d
Sachin <sachudubey143@gmail.com>
2025-06-10 13:50:00 +0530

    kivy layout

--- layout.py
+++ layout.py
@@ -5,10 +5,14 @@ from kivy.uix.boxlayout import BoxLayout
 class HelloApp(App):
     def build(self):
         lo=BoxLayout(orientation='vertical')
+        loo=BoxLayout(orientation='horizontal')
         l1=Label(text="Hello World",font_size=50)
         l2=Label(text="This is my page",font_size=50,color=(1,0,0,1))
+        l3=Label(text="Horizontal page ", font_size=60, color=(1,1,0,1))
         lo.add_widget(l1)
         lo.add_widget(l2)
+        loo.add_widget(l3)
+        lo.add_widget(loo)
         return lo
 
 app = HelloApp()
